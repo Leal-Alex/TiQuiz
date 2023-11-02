5aa84cb406ac5fc1983ba3089b3b2a77136d3b9f
Diogo Alex Leal <alexleal004@proton.me>
2023-11-02 07:12:50 -0300

    improviments

--- Source/main.js
+++ Source/main.js
@@ -81,26 +81,77 @@ const perguntas = [
     
 ];
 
-// Obtém elementos HTML
 const perguntaElement = document.getElementById('pergunta');
 const imagemElement = document.getElementById('imagem');
 const alternativasElement = document.getElementById('alternativas');
 const botaoJogar = document.getElementById('jogar');
 const perguntaContainer = document.querySelector('.pergunta-container');
+const temporizadorElement = document.getElementById('temporizador');
 
-botaoJogar.addEventListener('click', function() {
-    // Oculta a tela inicial
-    this.parentElement.style.display = 'none';
+let perguntaAtual = 0;
+let tempoRestante = 10; // Defina o tempo desejado em segundos
 
-    // Mostra a tela das perguntas
+botaoJogar.addEventListener('click', iniciarJogo);
+
+function iniciarJogo() {
+    botaoJogar.parentElement.style.display = 'none';
     perguntaContainer.style.display = 'block';
-
-    // Inicie o jogo
     mostrarProximaPergunta();
-});
+    atualizarTemporizador();
+}
 
-let perguntaAtual = 0; // Índice da pergunta atual
+function atualizarTemporizador() {
+    temporizadorElement.textContent = `Tempo Restante: ${tempoRestante}`;
 
+    if (tempoRestante === 0) {
+        verificarResposta('');
+    } else {
+        tempoRestante--;
+        setTimeout(atualizarTemporizador, 1000);
+    }
+}
+
+function mostrarProximaPergunta() {
+    if (perguntaAtual < perguntas.length) {
+        const pergunta = perguntas[perguntaAtual];
+        perguntaElement.textContent = pergunta.pergunta;
+        imagemElement.src = pergunta.imagem;
+        alternativasElement.innerHTML = '';
+
+        pergunta.alternativas.forEach((alternativa) => {
+            const botao = document.createElement('button');
+            botao.textContent = alternativa;
+            botao.className = 'alternativa';
+            botao.addEventListener('click', () => verificarResposta(alternativa));
+            alternativasElement.appendChild(botao);
+        });
+    } else {
+        encerrarJogo();
+    }
+}
+
+function verificarResposta(resposta) {
+    const pergunta = perguntas[perguntaAtual];
+
+    if (resposta === pergunta.resposta_correta) {
+        alert('Parabéns, você acertou!');
+    } else {
+        alert('Infelizmente, você errou.');
+    }
+
+    perguntaAtual++;
+    tempoRestante = 10; // Reinicie o temporizador
+    mostrarProximaPergunta();
+}
+
+function encerrarJogo() {
+    perguntaElement.textContent = 'Fim do Jogo';
+    imagemElement.src = '';
+    alternativasElement.innerHTML = '';
+    temporizadorElement.textContent = 'Tempo Restante: 10';
+}
+
+// Embaralhar as perguntas para tornar o jogo mais interessante
 embaralharPerguntas(perguntas);
 
 function embaralharPerguntas(array) {
@@ -108,54 +159,5 @@ function embaralharPerguntas(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-}
-
-// Função para exibir a próxima pergunta
-function mostrarProximaPergunta() {
-    if (perguntaAtual < perguntas.length) {
-        const pergunta = perguntas[perguntaAtual];
-
-        // Exibe a pergunta
-        perguntaElement.textContent = pergunta.pergunta;
-
-        // Exibe a imagem
-        imagemElement.src = pergunta.imagem;
-
-        // Exibe as alternativas
-        alternativasElement.innerHTML = ''; // Limpa as alternativas anteriores
-
-        pergunta.alternativas.forEach((alternativa) => {
-            const botao = document.createElement('button');
-            botao.textContent = alternativa;
-            botao.className = 'alternativa'; // Adicione a classe 'alternativa'
-
-            // Adiciona um evento de clique para verificar a resposta
-            botao.addEventListener('click', () => verificarResposta(alternativa));
-
-            alternativasElement.appendChild(botao);
-        });
-    } else {
-        // Fim do jogo
-        perguntaElement.textContent = 'Fim do Jogo';
-        imagemElement.src = '';
-        alternativasElement.innerHTML = '';
-    }
-}
-
-// Função para verificar a resposta do jogador
-function verificarResposta(resposta) {
-    const pergunta = perguntas[perguntaAtual];
-
-    if (resposta === pergunta.resposta_correta) {
-        // Resposta correta
-        alert('Parabéns, você acertou!');
-    } else {
-        // Resposta incorreta
-        alert('Infelizmente, você errou.');
-    }
-
-    // Mostra a próxima pergunta
-    perguntaAtual++;
-    mostrarProximaPergunta();
 }
 
