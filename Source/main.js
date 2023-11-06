1b5ac60b91e8375ca68df1929ff85d23cd0cb9fd
Diogo Alex Leal <alexleal004@proton.me>
2023-11-06 00:19:42 -0300

    95%Done

--- Source/main.js
+++ Source/main.js
@@ -13,11 +13,108 @@ const questions = [
         correctAnswer: 'Bill Gates'
     },
     {
-        image: '/Imagens_PP/billgates.jpg',
+        image: '/Imagens_PP/amd.jpg',
         question: 'O que significa AMD?',
         answers: ['American Micro Devices', 'Advanced Micro Devices', 'All Modern Devices', 'Association of Micro Devices'],
         correctAnswer: 'Advanced Micro Devices'
     },
+    {
+        image: '/Imagens_PP/processador.jpg',
+        question: 'O que significa CPU?',
+        answers: ['Central Processing Unit', 'Computer Peripheral Unit', 'Central Power Unit', 'Computer Programming Utility'],
+        correctAnswer: 'Central Processing Unit'
+    },
+    {
+        image: '/Imagens_PP/placadevideo.jpg',
+        question: 'O que significa GPU?',
+        answers: [' General Purpose Unit', 'Graphics Processing Unit', 'Global Positioning Unit', 'General Product Utilization'],
+        correctAnswer: 'Graphics Processing Unit'
+    },
+    {
+        image: '/Imagens_PP/boolean-pog.jpg',
+        question: 'Qual lógica foi criada por George Boole?',
+        answers: ['Lógica de Aristóteles', 'Lógica Binária', 'Lógica Difusa', 'Lógica Booleana'],
+        correctAnswer: 'Lógica Booleana'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+    {
+        image: '/Imagens_PP/valesilicio.jpg',
+        question: 'Onde fica o maior polo de tecnologia do mundo?',
+        answers: ['Vale do Silício, Califórnia', 'Tóquio, Japão', 'Londres, Reino Unido', 'Nova Iorque, EUA'],
+        correctAnswer: 'Vale do Silício, Califórnia'
+    },
+    {
+        image: '/Imagens_PP/fugaku.jpg',
+        question: 'Qual o computador mais potente do mundo?',
+        answers: ['Apple MacBook Air', 'IBM PC XT', 'Supercomputador Fugaku', 'Compaq Presario'],
+        correctAnswer: 'Supercomputador Fugaku'
+    },
+    {
+        image: '/Imagens_PP/patinhofeio.jpg',
+        question: 'Qual o nome do primeiro computador totalmente brasileiro?',
+        answers: [' CCE-7100', 'Patinho Feio', 'Sempron 64', 'O Pinguim'],
+        correctAnswer: 'Patinho Feio'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Para que serve o Cooler?',
+        answers: ['Para aquecer o computador', 'Para aumentar a luminosidade do monitor', 'Para resfriar componentes, como o processador', 'Para refrigerar bebidas'],
+        correctAnswer: 'Para resfriar componentes eletrônicos, como o processador'
+    },
+    {
+        image: '/Imagens_PP/html.jpg',
+        question: 'O que significa a sigla HTML?',
+        answers: ['HyperText Markup Language', 'High-Tech Multimedia Language', 'Human Technology Management Language', ' Hardware and Tools Markup Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        image: '/Imagens_PP/android.jpg',
+        question: 'Qual empresa é conhecida por desenvolver o sistema operacional Android?',
+        answers: ['Apple', 'Google', 'Microsoft', 'Samsung'],
+        correctAnswer: 'Google'
+    },
+    {
+        image: '/Imagens_PP/url.jpg',
+        question: 'O que significa URL?',
+        answers: ['Uniform Resource Locator', 'Universal Remote Link', 'User Registration Language', 'Ultra Rapid Loading'],
+        correctAnswer: 'Uniform Resource Locator'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+    {
+        image: '/Imagens_PP/primeiragpu.jpg',
+        question: 'Quando foi lançada a primeira GPU?',
+        answers: ['1980', '1995', '2005', '1972'],
+        correctAnswer: '1972'
+    },
+
     
 ];
 
