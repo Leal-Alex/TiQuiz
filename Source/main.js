140f1ac6746efdc2704ae696a4adcadef58298a7
Diogo Alex Leal <alexleal004@proton.me>
2023-11-07 21:25:52 -0300

    checkFolder

--- Source/main.js
+++ Source/main.js
@@ -1,163 +1,163 @@
 // Definir perguntas, cada pergunta é um objeto com uma imagem, a pergunta e as alternativas
 const questions = [
     {
-        image: '/Imagens_PP/alanturing.jpg',
+        image: '../Imagens_PP/alanturing.jpg',
         question: 'Quem é considerado o pai da computação?',
         answers: ['Alan Turing', 'Bill Gates', 'Steve Jobs', 'Mark Zuckerberg'],
         correctAnswer: 'Alan Turing'
     },
     {
-        image: '/Imagens_PP/billgates.jpg',
+        image: '../Imagens_PP/billgates.jpg',
         question: 'Quem criou o Sistema Operacional Windows?',
         answers: ['Linus Torvalds', 'Bill Gates', 'Steve Jobs', 'Mark Zuckerberg'],
         correctAnswer: 'Bill Gates'
     },
     {
-        image: '/Imagens_PP/amd.jpg',
+        image: '../Imagens_PP/amd.jpg',
         question: 'O que significa AMD?',
         answers: ['American Micro Devices', 'Advanced Micro Devices', 'All Modern Devices', 'Association of Micro Devices'],
         correctAnswer: 'Advanced Micro Devices'
     },
     {
-        image: '/Imagens_PP/cpu.jpg',
+        image: '../Imagens_PP/cpu.jpg',
         question: 'O que significa CPU?',
         answers: ['Central Processing Unit', 'Computer Peripheral Unit', 'Central Power Unit', 'Computer Programming Utility'],
         correctAnswer: 'Central Processing Unit'
     },
     {
-        image: '/Imagens_PP/placadevideo.jpg',
+        image: '../Imagens_PP/placadevideo.jpg',
         question: 'O que significa GPU?',
         answers: [' General Purpose Unit', 'Graphics Processing Unit', 'Global Positioning Unit', 'General Product Utilization'],
         correctAnswer: 'Graphics Processing Unit'
     },
     {
-        image: '/Imagens_PP/boolean-pog.jpg',
+        image: '../Imagens_PP/boolean-pog.jpg',
         question: 'Qual lógica foi criada por George Boole?',
         answers: ['Lógica de Aristóteles', 'Lógica Binária', 'Lógica Difusa', 'Lógica Booleana'],
         correctAnswer: 'Lógica Booleana'
     },
     {
-        image: '/Imagens_PP/primeiragpu.jpg',
+        image: '../Imagens_PP/primeiragpu.jpg',
         question: 'Quando foi lançada a primeira GPU?',
         answers: ['1980', '1995', '2005', '1972'],
         correctAnswer: '1972'
     },
     {
-        image: '/Imagens_PP/valesilicio.jpg',
+        image: '../Imagens_PP/valesilicio.jpg',
         question: 'Onde fica o maior polo de tecnologia do mundo?',
         answers: ['Vale do Silício, Califórnia', 'Tóquio, Japão', 'Londres, Reino Unido', 'Nova Iorque, EUA'],
         correctAnswer: 'Vale do Silício, Califórnia'
     },
     {
-        image: '/Imagens_PP/fugaku.jpg',
+        image: '../Imagens_PP/fugaku.jpg',
         question: 'Qual o computador mais potente do mundo?',
         answers: ['Apple MacBook Air', 'IBM PC XT', 'Supercomputador Fugaku', 'Compaq Presario'],
         correctAnswer: 'Supercomputador Fugaku'
     },
     {
-        image: '/Imagens_PP/patinhofeio.jpg',
+        image: '../Imagens_PP/patinhofeio.jpg',
         question: 'Qual o nome do primeiro computador totalmente brasileiro?',
         answers: [' CCE-7100', 'Patinho Feio', 'Sempron 64', 'O Pinguim'],
         correctAnswer: 'Patinho Feio'
     },
     {
-        image: '/Imagens_PP/cooler.jpg',
+        image: '../Imagens_PP/cooler.jpg',
         question: 'Para que serve o Cooler?',
         answers: ['Para aquecer o computador', 'Para aumentar a luminosidade do monitor', 'Para resfriar componentes, como o processador', 'Para refrigerar bebidas'],
         correctAnswer: 'Para resfriar componentes, como o processador'
     },
     {
-        image: '/Imagens_PP/html.jpg',
+        image: '../Imagens_PP/html.jpg',
         question: 'O que significa a sigla HTML?',
         answers: ['HyperText Markup Language', 'High-Tech Multimedia Language', 'Human Technology Management Language', ' Hardware and Tools Markup Language'],
         correctAnswer: 'HyperText Markup Language'
     },
     {
-        image: '/Imagens_PP/android.jpg',
+        image: '../Imagens_PP/android.jpg',
         question: 'Qual empresa é conhecida por desenvolver o sistema operacional Android?',
         answers: ['Apple', 'Google', 'Microsoft', 'Samsung'],
         correctAnswer: 'Google'
     },
     {
-        image: '/Imagens_PP/url.jpg',
+        image: '../Imagens_PP/url.jpg',
         question: 'O que significa URL?',
         answers: ['Uniform Resource Locator', 'Universal Remote Link', 'User Registration Language', 'Ultra Rapid Loading'],
         correctAnswer: 'Uniform Resource Locator'
     },
     {
-        image: '/Imagens_PP/js.jpg',
+        image: '../Imagens_PP/js.jpg',
         question: 'Qual é a linguagem de programação mais usada para desenvolvimento de websites?',
         answers: ['C++', 'JavaScript', 'Excel', 'Python'],
         correctAnswer: 'JavaScript'
     },
     {
-        image: '/Imagens_PP/bluetooth.jpg',
+        image: '../Imagens_PP/bluetooth.jpg',
         question: 'O que é Bluetooth?',
         answers: ['Uma marca de refrigerantes', 'Uma tecnologia de conexão sem fio para dispositivos', 'Um tipo de música', 'Uma rede social'],
         correctAnswer: 'Uma tecnologia de conexão sem fio para dispositivos'
     },
     {
-        image: '/Imagens_PP/email.jpg',
+        image: '../Imagens_PP/email.jpg',
         question: 'O que é um e-mail?',
         answers: ['Um animal de estimação', 'Uma caixa de correspondências', 'Uma mensagem eletrônica enviada pela internet', 'Um prato de comida'],
         correctAnswer: 'Uma mensagem eletrônica enviada pela internet'
     },
     {
-        image: '/Imagens_PP/placamae.jpg',
+        image: '../Imagens_PP/placamae.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['Processador', 'Placa Pai', 'Placa Mãe', 'Chipset'],
         correctAnswer: 'Placa Mãe'
     },
     {
-        image: '/Imagens_PP/processador.jpg',
+        image: '../Imagens_PP/processador.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['Memória RAM', 'Processador', 'Placa Mãe', 'Chipset'],
         correctAnswer: 'Processador'
     },
     {
-        image: '/Imagens_PP/ram.jpg',
+        image: '../Imagens_PP/ram.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['Memória RAM', 'Placa de Video', 'SSD', 'VRM'],
         correctAnswer: 'Memória RAM'
     },
     {
-        image: '/Imagens_PP/gabinete.jpg',
+        image: '../Imagens_PP/gabinete.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['CPU', 'Gabinete', 'Box', 'PC'],
         correctAnswer: 'Gabinete'
     },
     {
-        image: '/Imagens_PP/fan.png',
+        image: '../Imagens_PP/fan.png',
         question: 'Identifique a imagem abaixo:',
         answers: ['Fan', 'Cooler', 'Ventilador', 'Hélice'],
         correctAnswer: 'Fan'
     },
     {
-        image: '/Imagens_PP/fonteatx.jpg',
+        image: '../Imagens_PP/fonteatx.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['Fonte ATX', 'Estabilizador', 'Condutor elétrico', 'Chipset'],
         correctAnswer: 'Fonte ATX'
     },
     {
-        image: '/Imagens_PP/SSD.jpg',
+        image: '../Imagens_PP/SSD.jpg',
         question: 'O que significa SSD?',
         answers: ['Solid State Driver', 'Sistema de Segurança Digital', 'Serviço de Streaming de Dados', 'Super Sistema de Download'],
         correctAnswer: 'Solid State Driver'
     },
     {
-        image: '/Imagens_PP/watercooler.jpg',
+        image: '../Imagens_PP/watercooler.jpg',
         question: 'Identifique a imagem abaixo:',
         answers: ['Processador', 'Air Cooler', 'Water Cooler', 'Cano de Resfriamento'],
         correctAnswer: 'Water Cooler'
     },
     {
-        image: '/Imagens_PP/firewall.jpg',
+        image: '../Imagens_PP/firewall.jpg',
         question: 'O que é firewall?',
         answers: ['Proteção de rede', 'Programa de edição', 'Dispositivo de cozinha', 'Estilo de dança'],
         correctAnswer: 'Proteção de rede'
     },
     {
-        image: '/Imagens_PP/rammemory.jpg',
+        image: '../Imagens_PP/rammemory.jpg',
         question: 'O que é RAM?',
         answers: ['Memória de acesso aleatório', 'Rádio AM', 'Refrigerante', 'Moda'],
         correctAnswer: 'Memória de acesso aleatório'
